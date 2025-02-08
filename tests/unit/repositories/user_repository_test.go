58db7074db805bfe5876d91acdd3c4d2b21db244
Aleksey <a.e.sokolkov@gmail.com>
2025-02-09 00:04:58 +0500

    feat: user repository tests template

---
+++ tests/unit/repositories/user_repository_test.go
@@ -0,0 +1,27 @@
+package repositories_test
+
+import "testing"
+
+func TestGetByUsernameValid(t *testing.T) {
+
+}
+
+func TestGetByUsernameInvalid(t *testing.T) {
+
+}
+
+func TestCreateValid(t *testing.T) {
+
+}
+
+func TestCreateInvalid(t *testing.T) {
+
+}
+
+func TestUpdateValid(t *testing.T) {
+
+}
+
+func TestUpdateInvalid(t *testing.T) {
+
+}
