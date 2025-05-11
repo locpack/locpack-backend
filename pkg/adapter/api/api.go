6c139e2743b693ccb7df13dafac176d589e13dd6
Aleksey <a.e.sokolkov@gmail.com>
2025-05-11 20:08:27 +0500

    feat: cors

--- pkg/adapter/api/api.go
+++ pkg/adapter/api/api.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"locpack-backend/pkg/adapter"
 	"locpack-backend/pkg/cfg"
@@ -8,5 +9,11 @@ import (
 
 func New(cfg *cfg.API) adapter.API {
 	gin.SetMode(cfg.Mode)
-	return gin.Default()
+	router := gin.Default()
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	config.AllowCredentials = true
+	config.AddAllowHeaders("*")
+	router.Use(cors.New(config))
+	return router
 }
